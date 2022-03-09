5ae6e80b28404fcd94a9ac24e2a31960be1a53ef
Mike Kelly <mike.forge@gmail.com>
2022-03-09 17:19:14 -0500

    added route to push new content to the json file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const file = require('./db/db.json');
 
 const port = process.env.PORT || 3001
 
@@ -15,7 +16,7 @@ console.log(`App is running at http://localhost:${port}`)
 app.use(express.json()); //interact with json 
 app.use(express.static('public')); //making public accessible
 
-//setup for routes
+//setup for api routes
 
 app.get('/api/notes', (req,res) => {
     let rawJson = fs.readFileSync('./db/db.json');
@@ -23,8 +24,29 @@ app.get('/api/notes', (req,res) => {
     console.log(JSON.parse(rawJson))
 })
 
+app.post('/api/notes', (req, res) => {
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        let note = req.body;
+        console.log(`this is the req.body: ${req.body}`);
+        file.push(note);
 
+        fs.writeFile('./db/db.json', JSON.stringify(file), err => {
+            if (err) throw err;
+            console.log(`success on writing new note`);
+        })
+    });
+});
+
+
+
+
+//html routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
